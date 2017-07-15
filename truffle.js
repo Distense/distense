b27e35cfde9c36b44eb70d0f47045a697a4a60a6
JohnAllen <johnjalleniii@gmail.com>
2017-07-15 12:32:19 +0200

    Add ropsten to truffle.js config

---
+++ truffle.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+ ropsten: {
+     host: "localhost",
+      port: 8545,
+      network_id:3 ,
+      gas:300000 
+ }
+  }};
