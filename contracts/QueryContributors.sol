d182f7ddc864a4dd08d2ba3ed5bb8ef334b3b957
JohnAllen <johnjalleniii@gmail.com>
2017-08-02 20:29:27 -0700

    Update  QueryContributors
    Distense: 581513ae9180217f25e9b3c502413c7a8a0733ec

--- contracts/QueryContributors.sol
+++ contracts/QueryContributors.sol
@@ -1,25 +1,41 @@
 pragma solidity ^0.4.11;
 
 import "./lib/oraclizeAPI_0.4.sol";
+import "./lib/Ownable.sol";
+import "./lib/solidity-stringutils/strings.sol";
 
-contract QueryContributors is usingOraclize {
 
-  event LogContribution(string indexed githubUserName);
+contract QueryContributors is usingOraclize, Ownable {
+
+  uint public interval;
+  bytes32 repo;
+  uint id;
+
+  mapping (uint => string) queryIDs;
+  event LogContribution(string indexed message, bytes32 indexed username, bytes32 url);
 
   function QueryContributors()  {
-    getLatestContributors();
+    interval = 30;
+    getLatestContribution();
   }
 
-  function __callback() {
+  function __callback(bytes32 queryID, string result) {
     require(msg.sender == oraclize_cbAddress());
+    if (contains(queryID, numberQueryType)) {
 
+    }
     LogContribution(result);
-    getLatestContributors();
+    getLatestContribution();
   }
 
-  function getLatestContributors()  {
-    oraclize_query(60, "URL", "json(https://api.github.com/repos/Distense/contracts/pulls)");
+  function updateInterval(newInterval) onlyOwner {
+    interval = newInterval;
   }
+
+  function getLatestContribution() payable {
+    userIDQuery = oraclize_query(interval, "URL", "json(https://api.github.com/repos/Distense/contracts/pulls).[0].user.login");
+  }
+
 }
 
 
