cdb257b29dc3511b0616fb29d8e3fb3424175d6f
Conner Petzold <cpetzold@gmail.com>
2017-08-02 22:58:05 -0700

    Pull request reward oracle

--- contracts/Distense.sol
+++ contracts/Distense.sol
@@ -18,23 +18,24 @@ contract Distense {
   }
 
   struct Task {
-    bytes32 id;
+    string id;
     uint256 reward;
+    bool rewarded;
   }
 
   mapping(address => bool) public approvedAddresses;
-  mapping(bytes32 => Task) public tasks;
+  mapping(string => Task) public tasks;
   mapping(address => Contributor) public contributors;
   mapping(bytes32 => address) public emailToAddress;
   
   event LogContributionReward(address indexed to, uint256 numDID);
 
-  function Distense(address approvedAddress) {
+  function Distense(address _approvedAddress) {
     totalSupply = 0;
     name = "Distense DID";
     symbol = "DID";
     owner = msg.sender;
-    QueryContributorAddress = approvedAddress;
+    QueryContributorAddress = _approvedAddress;
   }
 
   function associateAccount(bytes32 _email) {
@@ -48,28 +49,37 @@ contract Distense {
   }
 
   function mintReward(address _to, uint256 _amount) onlyApprovedAddresses returns (bool) {
-    require(msg.sender == QueryContributorAddress);
     totalSupply = totalSupply.add(_amount);
     contributors[_to].balance = contributors[_to].balance.add(_amount);
     LogContributionReward(_to, _amount);
     return true;
   }
 
-  function setTaskReward(bytes32 id, uint256 number) onlyApprovedAddresses {
-    tasks[id] = Task(id, number);
+  function rewardTask(address _to, string _taskId) onlyApprovedAddresses {
+    require(!isTaskRewarded(_taskId));
+    mintReward(_to, tasks[_taskId].reward);
+  }
+
+  function isTaskRewarded(string _id) returns (bool) {
+    return tasks[_id].rewarded;
+  }
+
+  function setTaskReward(string _id, uint256 _reward) onlyApprovedAddresses {
+    require(!isTaskRewarded(_id));
+    tasks[_id] = Task(_id, _reward, false);
   }
 
   function balanceOf(address _owner) constant returns (uint256 balance) {
     return contributors[_owner].balance;
   }
 
-  function transferOwnership(address newOwner) onlyOwner {
-    require(newOwner != address(0));
-    owner = newOwner;
+  function transferOwnership(address _newOwner) onlyOwner {
+    require(_newOwner != address(0));
+    owner = _newOwner;
   }
 
-  function addApprovedAddress(address validAddress) onlyOwner {
-    approvedAddresses[validAddress] = true;
+  function addApprovedAddress(address _validAddress) onlyOwner {
+    approvedAddresses[_validAddress] = true;
 
   }
 
