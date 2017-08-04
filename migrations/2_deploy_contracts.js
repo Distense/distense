b652719b353574b54722b9b5e76e05a0a2b1baec
JohnAllen <johnjalleniii@gmail.com>
2017-08-03 18:39:43 -0700

    Update 2_migrations to add Contributors address once deployed
    Distense: 581513ae9180217f25e9b3c502413c7a8a0733ec

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,12 @@ const Contributions = artifacts.require('./Contributions.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(Distense);
-  deployer.deploy(Contributions);
+  console.log(`Distense.address: ${Distense.address}`);
+  deployer.deploy(Contributions, Distense.address);
+  deployer.then(function() {
+    return Distense.new();
+  }).then(function(DistenseInstance) {
+    console.log(`Contributions.address: ${Contributions.address}`);
+    return DistenseInstance.addApprovedAddress(Contributions.address);
+  });
 };
