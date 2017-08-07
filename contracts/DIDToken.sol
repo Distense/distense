bebdc6ed865ad643ca59ace41a00fdf450c1a6fe
JohnAllen <johnjalleniii@gmail.com>
2017-08-07 16:26:45 -0700

    Updates

--- contracts/DIDToken.sol
+++ contracts/DIDToken.sol
@@ -2,29 +2,33 @@ pragma solidity ^0.4.11;
 
 import './lib/Approvable.sol';
 import './lib/SafeMath.sol';
+import './HAVToken.sol';
 
 contract DIDToken is Approvable {
   using SafeMath for uint256;
 
+  address public owner;
   string public name;
   string public symbol;
   uint256 public numDIDOutstanding;
   uint256 public numContributors;
   uint8 public decimals;
-  mapping (address => uint) public balances;
-
+  string public taskID;
+  address HAVAddress;
+  mapping (address => uint) public DIDBalances;
 
   struct Contributor {
-    uint256 balance;
+    uint256 DIDBalance;
     string email;
     bytes8 countryCode;
   }
   mapping(address => Contributor) public contributors;
   mapping(bytes32 => address) public emailToAddress;
 
-  event LogDIDReward(address indexed to, uint256 numDID, bytes32 taskID);
+  event LogDIDReward(address indexed to, uint256 numDID, string indexed taskID);
 
   function DIDToken () {
+    owner = msg.sender;
     name = "Distense DID";
     symbol = "DID";
     numDIDOutstanding = 0;
@@ -32,40 +36,51 @@ contract DIDToken is Approvable {
     decimals = 1;
   }
 
-  function associateAccount(bytes32 _email) {
-    require(emailToAddress[_email] == 0);
-    emailToAddress[_email] = msg.sender;
-  }
-
   function updateProfile(string _email, bytes8 _countryCode) {
     contributors[msg.sender].email = _email;
     contributors[msg.sender].countryCode = _countryCode;
   }
 
-  function mint(address _to, uint256 _amount) /*onlyContributorContract TODO */ returns (bool) {
-    totalSupplyDID = totalSupplyDID.add(_amount);
-    contributors[_to].balance = contributors[_to].balance.add(_amount);
-    balances[_to] = balances[_to] + _amount;
-    LogDIDReward(_to, _amount);
+  function issueDIDReward(address _contribAddress, uint256 _amount, string _taskID) internal returns (bool) {
+    numDIDOutstanding = numDIDOutstanding.add(_amount);
+    contributors[_contribAddress].DIDBalance = contributors[_contribAddress].DIDBalance.add(_amount);
+    DIDBalances[_contribAddress] = DIDBalances[_contribAddress] + _amount;
+    LogDIDReward(_contribAddress, _amount, _taskID);
     return true;
   }
 
   // This is called from HAVToken to decrement DID hodler's account here prior to issuing HAV when they exchange
-  function exchangeDIDForHAV(address _to, uint256 _amount) {
-    require(balances[_to]); // sanity check; prevent false burns
-    require(_amount <= contributors[_to].balance);
-    totalSupplyDID = totalSupply.sub(_amount);
-    contributors[_to].balance = contributors[_to].balance.subtract(_amount);
-    LogContributionReward(_to, _amount);
+  function exchangeDIDForHAV(address _contribAddress, uint256 _amount) onlyHAVContract returns (bool) {
+    require(DIDBalances[_contribAddress] >= _amount);
+    require(_amount <= contributors[_contribAddress].DIDBalance);
+    numDIDOutstanding = numDIDOutstanding.sub(_amount);
+    contributors[_contribAddress].DIDBalance = contributors[_contribAddress].DIDBalance.sub(_amount);
     return true;
   }
 
-  function incrementNumContributors() {
+  // TODO this is probably unecessary but leave here for now
+  //  when DID for HAV exchanges are UNsuccessful, we revert the DID burn that occurs in exchangeDIDForHAV()
+//  function reverseDIDForHAV(address _contribAddress, uint256 _amount) onlyHAVContract {
+//    numDIDOutstanding = numDIDOutstanding.add(_amount);
+//    contributors[_contribAddress].DIDBalance = contributors[_contribAddress].DIDBalance.add(_amount);
+//    return true;
+//  }
+
+  function incNumContributors() {
     numContributors += 1;
   }
 
-  function balanceOf(address _owner) constant returns (uint256 balance) {
-    return contributors[_owner].balance;
+  function balanceOf(address _contribAddress) constant returns (uint256 balance) {
+    return contributors[_contribAddress].DIDBalance;
+  }
+
+  function setHAVAddress(address _HAVAddress) onlyOwner returns (bool) {
+    HAVAddress = _HAVAddress;
+  }
+
+  modifier onlyHAVContract {
+    require(msg.sender == HAVAddress);
+    _;
   }
 
 }
--- contracts/HAVToken.sol
+++ contracts/HAVToken.sol
@@ -1,34 +1,31 @@
 // Distense is a decentralized, for-profit code cooperative
 // Anyone can contribute
 
-// INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE/INSECURE
+// INSECURE/DRAFT
 pragma solidity ^0.4.11;
 
+import './DIDToken.sol';
+import './lib/Approvable.sol';
+import './lib/SafeMath.sol';
 
-import '../math/SafeMath.sol';
 
-
-//  TODO what is the rate/number per ether? This must coincide approximately with DID rate per hour
+//  TODO what is initial the rate/number per ether? This must coincide approximately with DID rate per hour
 //  TODO add link to page about Distense and the HAV token
 //  TODO what address should receive the ether if not this contract's?
-//  TODO should we offer the ability of a purchaser to buy for a beneficiary
 //  TODO what other limitations should be placed on validPurchase()?
 //  TODO are the units ok?  Can we just
 //  TODO migration options?
 //  TODO should we limit/hardcode a max % of DID?
 //  TODO should we forwardFunds(); somewhere or leave funds in this contract?
+//  TODO should we start the sale after some number of blocks?
 
+// INSECURE/DRAFT
 contract HAVToken {
   using SafeMath for uint256;
 
-  event Mint(address indexed to, uint256 amount);
-
-  event TokenPurchase(address indexed purchaser, uint256 value, uint256 amount);
-
-  event LogHAVSaleStatusChange(bool saleEnabled);
-
-  mapping (address => uint256) public balancesHAV;
-
+  address public owner = msg.sender;
+  string public name;
+  string public symbol;
   uint256 public numHAVOutstanding;
   address public DIDTokenAddress;
   uint256 public maxBalanceEther;
@@ -36,34 +33,33 @@ contract HAVToken {
   uint256 public numHAVForSale;
   uint256 public cumEtherRaised;
   address public wallet;
-  uint256 public initialHAVPerEther;  // "initial" because once there is a market we will have to adjust sale price to market price
+  uint256 public HAVPerEther;  // "initial" because once there is a market we will have to adjust sale price to market price
   bool public tradingMarketExists;
   bool saleActive;
 
+  mapping (address => uint256) public balancesHAV;
+
+  event DIDHAVExchange(address indexed purchaser, uint256 amount);
+  event HAVPurchase(address indexed purchaser, uint256 value, uint256 amount);
+  event LogHAVSaleStatusChange(bool saleActive);
 
   function HAVToken (address _wallet) {
 
     name = "Distense HAV";
     symbol = "HAV";
-    numHAVOutstanding = 0;
-    numContributors = 0;
-
-    wallet = _wallet; // TODO
-    maximumBalanceEther = maxBalanceEther * 1 ether;
-    initialHAVPerEther = 200;
+    numHAVOutstanding = 314; // For UI testing
+    wallet = _wallet; // TODO research
+    maxBalanceEther = maxBalanceEther * 1 ether;  // TODO is this right?  basically a type conversion behind the scenes?
+    HAVPerEther = 200;
 
     require(maxBalanceEther > 0);
     require(HAVPerEther > 0);
     require(_wallet != 0x0);
   }
 
-  function exchangeHAVForDID(address burner) onlyDIDTokenAddress {
-
-  }
-
   function validPurchase() internal constant returns (bool) {
     bool withinCap = currentBalanceEther.add(msg.value) <= maxBalanceEther;
-    //        if (withinCap)  return true;
+    return withinCap;
   }
 
   // fallback function to buy tokens
@@ -76,28 +72,27 @@ contract HAVToken {
     require(approvePurchase());
 
     uint256 weiAmount = msg.value;
-
-    // calculate token amount to be created
     uint256 numHAVTokens = weiAmount.mul(HAVPerEther);
+    cumEtherRaised = cumEtherRaised.add(weiAmount);
 
-    // update state
-    cumulativeEtherRaised = cumulativeEtherRaised.add(weiAmount);
-
-    mint(beneficiary, numHAVTokens);
+    issueHAVForDID(beneficiary, numHAVTokens);
     HAVPurchase(msg.sender, weiAmount, numHAVTokens);
   }
 
-  function mint(address _to, uint256 _amount) internal onlyOwner returns (bool) {
+  function issueHAVForDID(address _to, uint256 _amount) internal returns (bool) {
 
-    bool burnDIDSuccess = DIDToken.exchangeDIDForHAV(_to, _amount);
+    // Reduce DID held prior to issuing HAV to prevent reentrancy (CRUCIAL/SECURITY)
+    DIDToken didToken = DIDToken(DIDTokenAddress);
+    bool burnDIDSuccess = didToken.exchangeDIDForHAV(_to, _amount);
     if (burnDIDSuccess) {
-      totalSupply = totalSupply.add(_amount);
-      balances[_to] = balances[_to].add(_amount);
-      Mint(_to, _amount);
+      numHAVOutstanding = numHAVOutstanding.add(_amount);
+      balancesHAV[_to] = balancesHAV[_to].add(_amount);
+      DIDHAVExchange(_to, _amount);
       return true;
-    } else {
-
-  }
+    }
+    else {
+      return false;
+    }
   }
 
   //  send ether to the fund collection wallet
@@ -108,22 +103,22 @@ contract HAVToken {
 
   // @return true if the transaction can buy tokens
   function approvePurchase() internal constant returns (bool) {
-    uint256 current = block.number;
     bool nonZeroPurchase = msg.value != 0;
     return nonZeroPurchase && saleActive;
   }
 
-  //  Contract sale price must be updated to reflect market price -- can't sell HAV here
-  //  if market exists and is trading > || < contract sale price
-  function tradingMarketExists(bool _tradingMarketExists) onlyOwner public returns (bool) {
+  //  If a trading market exists we need to update a sale price
+  function tradingMarketExists(bool _tradingMarketExists) public returns (bool) {
+    require(msg.sender == owner);
     tradingMarketExists = _tradingMarketExists;
   }
 
-  function numberAvailableForSale() external constant returns (uint256) {
+  function numberAvailableForSale() constant returns (uint256) {
     return maxBalanceEther - currentBalanceEther;
   }
 
-  function pauseSale() onlyOwner public returns (bool){
+  function pauseSale() public returns (bool) {
+    require(msg.sender == owner);
     saleActive = !saleActive;
     LogHAVSaleStatusChange(saleActive);
     return true;
