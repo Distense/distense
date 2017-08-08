2bd1f7a2269a3ecfac9a83aab75dee91f9f8c927
JohnAllen <johnjalleniii@gmail.com>
2017-08-08 14:46:00 -0700

    Update layout links

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 export default () => (
   <Layout>
     <div>
-      <Link route='profile' params={{ address: '0x098964520943bbf6731aa02ca9c7b1eb8c34e4f9' }}><a>Sample profile</a></Link>
+      <img src="./static/banner-logo.png"/>
     </div>
   </Layout>
 )
