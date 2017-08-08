b1120bed3890f9345701f6e55b9f5aa8f74d933b
JohnAllen <johnjalleniii@gmail.com>
2017-08-07 17:37:12 -0700

    Add constructor function to Distense.sol Add HAVToken to deployer script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const DIDToken = artifacts.require('./DIDToken.sol')
+const HAVToken = artifacts.require('./HAVToken.sol')
 const Distense = artifacts.require('./Distense.sol')
 
 module.exports = deployer => {
   deployer.deploy(DIDToken)
+  deployer.deploy(HAVToken)
   deployer.deploy(Distense)
 }
