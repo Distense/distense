e1802186d2a1de22a702a2b583b3f5808a701cd9
JohnAllen <johnjalleniii@gmail.com>
2017-08-14 18:27:43 -0700

    Add contributors files

---
+++ frontend/pages/Contributions.js
@@ -0,0 +1,64 @@
+import Layout from '../components/layout'
+import React, { Component } from 'react';
+import web3, { selectContractInstance } from '../blockchain/web3.js';
+
+
+import ContributorsContract from '../blockchain/Contributors.json';
+const ContributorsDeployedAddress = '0x098964520943bbf6731aa02ca9c7b1eb8c34e4f9';
+
+
+export default class Contributions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contributorsList: []
+    }
+  }
+
+  async componentWillMount() {
+
+    this.Contributors = await selectContractInstance(ContributorsContract, ContributorsDeployedAddress);
+    const contributorsList = await this.getContributors();
+    if (contributorsList.length) this.setState({ contributorsList });
+  }
+
+  async getContributors() {
+
+    const contributors = await this.Contributors.getContributors();
+
+    const names = [];
+    if (contributors) {
+      for (const contributor of contributors) {
+        const contrib = {
+          name: web3.toAscii(contributor).toUpperCase(),
+          address: contributor
+        };
+        names.push(contrib);
+      }
+    }
+    return names;
+  }
+
+  render() {
+    const s = this.state;
+    return (
+      <Layout title="Contributions">
+        <div className="contributors">
+          <div className="">
+            <h2>Contributors</h2>
+          </div>
+          {s.contributorsList.length ? s.contributorsList.map((contrib) => {
+            return <Contributor key={contrib.address} contrib={contrib}/>
+          }) : 'No contributors.'
+          }
+        </div>
+      </Layout>
+    );
+  }
+}
+
+const Contributor = (p) => (
+  <div className="contribution">
+    <p>{p.contrib.desc}: {p.contrib.url}</p>
+  </div>
+);
