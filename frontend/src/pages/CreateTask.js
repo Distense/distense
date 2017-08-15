05f92a06e2b34a9c09955e06a279536509ab1f85
JohnAllen <johnjalleniii@gmail.com>
2017-08-14 19:44:01 -0700

    Remove contribute link and add tasks links to Layout

--- frontend/src/pages/CreateTask.js
+++ frontend/src/pages/CreateTask.js
@@ -107,6 +107,31 @@ export default class CreateTask extends Component {
             </form>
           }
         </div>
+        <style jsx>{`
+          .button {
+            margin-top: 10px;
+            background: #4ad934;
+            background-image: -webkit-linear-gradient(top, #4ad934, #4eb82b);
+            background-image: -moz-linear-gradient(top, #4ad934, #4eb82b);
+            background-image: -ms-linear-gradient(top, #4ad934, #4eb82b);
+            background-image: -o-linear-gradient(top, #4ad934, #4eb82b);
+            background-image: linear-gradient(to bottom, #4ad934, #4eb82b);
+            -webkit-border-radius: 5;
+            -moz-border-radius: 5;
+            border-radius: 5px;
+            text-shadow: 2px 1px 3px #666666;
+            font-family: Courier New;
+            color: #ffffff;
+            font-size: 27px;
+            padding: 10px 20px 10px 20px;
+            text-decoration: none;
+          }
+
+          .button:hover {
+            background: #6cfc3c;
+            text-decoration: none;
+          }
+       `}</style>
       </Layout>
     );
   }
