38edef3f66728f5b74f876e1a943be513369e25b
JohnAllen <johnjalleniii@gmail.com>
2017-08-22 13:12:27 -0700

    Update Tags elements with better styles

--- frontend/src/pages/CreateTask.js
+++ frontend/src/pages/CreateTask.js
@@ -80,6 +80,186 @@ class CreateTask extends Component {
           </form>
           </div>
         </div>
+
+        { /*language=SCSS*/ }
+        <style jsx>{`
+          body * {
+            font-family: Quicksand;
+          }
+
+          :global(.Select-multi-value-wrapper) {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: center;
+          }
+
+          :global(.Select-value), :global(.select-option) {
+            background-color: lightgray;
+            border: 1px solid transparent !important;
+            border-radius: .25rem;
+            display: inline-block;
+            font-weight: 400;
+            line-height: 1.25;
+            margin: 2px;
+            padding: .4rem .6rem;
+            font-size: 1rem;
+            text-align: center;
+            text-transform: none;
+            vertical-align: middle;
+            user-select: none;
+            white-space: nowrap;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+          }
+
+          :global(.Select-menu-outer) {
+            margin-top: 5px;
+          }
+
+          :global(.Select-value-icon) {
+            margin-right: 4px;
+            cursor: pointer;
+          }
+
+          .task-create-view {
+	          display: flex;
+          }
+
+          .task-input-group {
+            margin-top: 15px;
+          }
+
+          .task-preview-content {
+            background: #FAEBD7;
+            border-radius: 3px;
+            font-size: 18px;
+            height: 270px;
+            padding: 10px;
+            width: 100%;
+          }
+
+          .task-create-inputs {
+            width: 38%;
+          }
+
+          .task-preview {
+            margin: auto;
+          }
+
+          .struct-line {
+            margin: 12px 0;
+          }
+
+          .bg-light-gray {
+            background-color: lightgray;
+          }
+
+          span.task-preview-value {
+            color: red;
+            padding: 4px;
+            border-radius: 3px;
+            -webkit-border-radius: 3;
+            -moz-border-radius: 3;
+            font-size: 13.5px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          }
+
+          .task-preview-key {
+            font-size: 16px;
+            margin: 3px 5px 3px 1rem;
+          }
+
+          p.error-message {
+            margin: .5em 0;
+            color: #fff;
+            padding: 4px;
+            width: 330px;
+            border-radius: 3px;
+            -webkit-border-radius: 3;
+            -moz-border-radius: 3;
+            background-color: red;
+          }
+
+          p.autocomplete-item {
+            fontFamily: '-apple-system, BlinkMacSystemFont, sans-serif';
+            padding: '.3rem';
+            fontSize: '18px';
+            borderBottom: '1px solid gray';
+          }
+
+          .word-separator {
+            margin: 0 6px;
+          }
+
+          .task-create-column {
+            width: 50%;
+	          padding: 10px;
+	        }
+
+	        .task-create-view > div:first-child {
+	          // margin-right: 5px;
+	        }
+
+          .input {
+            margin: 10px 0 20px 0;
+            border: 1px solid gray;
+            -webkit-border-radius: 5;
+            -moz-border-radius: 5;
+            border-radius: 5px;
+            width: 330px;
+          }
+
+          .inline {
+            display: inline;
+          }
+
+          input {
+            border: 1px solid gray !important;
+          }
+
+          .input-detail {
+            height: 100px;
+          }
+
+          .tx-hash {
+            overflow-wrap: break-word;
+          }
+
+          .button {
+            margin-top: 10px;
+            background: #4ad934;
+            background-image: -webkit-linear-gradient(top, #4ad934, #4eb82b);
+            background-image: -moz-linear-gradient(top, #4ad934, #4eb82b);
+            background-image: -ms-linear-gradient(top, #4ad934, #4eb82b);
+            background-image: -o-linear-gradient(top, #4ad934, #4eb82b);
+            background-image: linear-gradient(to bottom, #4ad934, #4eb82b);
+            -webkit-border-radius: 5;
+            -moz-border-radius: 5;
+            border-radius: 5px;
+            text-shadow: 2px 1px 3px #666666;
+            color: #ffffff;
+            font-size: 18px;
+            padding: 10px 20px;
+            text-decoration: none;
+          }
+
+          .button:hover {
+            background: #6cfc3c;
+            text-decoration: none;
+          }
+
+          input#project-autocomplete, input#sub-project-autocomplete {
+            -webkit-border-radius: 5;
+            -moz-border-radius: 5;
+            border-radius: 5px;
+            width: 330px !important;
+          }
+
+       `}</style>
       </Layout>
     )
   }
