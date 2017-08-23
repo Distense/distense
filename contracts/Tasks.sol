252163c1b913c37b5e58b8ef4305cef9f5f25921
JohnAllen <johnjalleniii@gmail.com>
2017-08-23 13:13:55 -0700

    Update Tasks.sol to accept tags, not project/subProject

--- contracts/Tasks.sol
+++ contracts/Tasks.sol
@@ -8,17 +8,14 @@ import './lib/SafeMath.sol';
 contract Tasks {
   using SafeMath for uint256;
 
-  uint256 public numContribs;
-
   address public votingAddress;
 
   struct Task {
     address createdBy;
     string title;
     string url;
-    string project;
-    string subProject;
-    bytes ipfsHashID; // longer than 32
+    bytes32[] tags;
+    bytes ipfsHashID; // longer than 32 so use bytes
     uint256 createdAt;
     TaskStatus status;
   }
@@ -28,8 +25,8 @@ contract Tasks {
 
   enum TaskStatus { Proposal, Task, Contribution }
 
-  function createTask(string _title, string _url, string _project, string _subProject, bytes _ipfsHashID) external returns (bool) {
-    Task memory task = Task(msg.sender, _title, _url, _project, _subProject, _ipfsHashID, block.timestamp, TaskStatus.Proposal);
+  function createTask(string _title, string _url, bytes32[] _tags, bytes _ipfsHashID) external returns (bool) {
+    Task memory task = Task(msg.sender, _title, _url, _tags, _ipfsHashID, block.timestamp, TaskStatus.Proposal);
     taskIds.push(_ipfsHashID);
     tasks[_ipfsHashID] = task;
     return true;
@@ -43,8 +40,6 @@ contract Tasks {
     address,
     string,
     string,
-    string,
-    string,
     uint256,
     TaskStatus
   ) {
@@ -55,8 +50,6 @@ contract Tasks {
     address,
     string,
     string,
-    string,
-    string,
     uint256,
     TaskStatus
   ) {
@@ -64,8 +57,6 @@ contract Tasks {
       tasks[_id].createdBy,
       tasks[_id].title,
       tasks[_id].url,
-      tasks[_id].project,
-      tasks[_id].subProject,
       tasks[_id].createdAt,
       tasks[_id].status
     );
