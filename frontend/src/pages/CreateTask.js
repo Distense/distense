736cc7059dee9433402c644c7356dc9209ee67b8
JohnAllen <johnjalleniii@gmail.com>
2017-08-31 17:56:46 -0700

    Rewrite CreateTask with Semantic UI Create shared constant file CreateTask flow now standard looking webpage

--- frontend/src/pages/CreateTask.js
+++ frontend/src/pages/CreateTask.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import CodeMirror from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/markdown/markdown'
-import Select from 'react-select'
+import { Dropdown } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router'
 import ReactMarkdown from 'react-markdown'
@@ -10,28 +10,11 @@ import slug from 'slug'
 
 import { getPendingTask } from '../reducers/tasks'
 import { createTask } from '../actions'
-
 import Head from '../components/common/Head'
 import Layout from '../components/Layout'
+import { tagsOptions, specPlaceholder } from '../shared'
 
 const taskUrl = ({ title, _id }) => `/tasks/${slug(title)}/${_id}`
-
-
-// Placeholder text for markdown spec input -- One line per array item
-const specPlaceholder = [
-  '\n## Sample Task Spec\n\n',
-  '- Make lists of pithy facts and instructions\n',
-  '- **Bold** stuff \n',
-  '- *Italicize* text\n',
-  '- ### Headers\n\n',
-  '## You are encouraged to add code examples\n\n',
-  '```js\n',
-  'var someVar = require(\'react\');\n',
-  'var Markdown = require(\'react-markdown\')\n\n',
-  'React.render(\n    <Markdown source=\'# Your markdown here\' />,\n     document.',
-  'getElementById(\'content\')\n)\n',
-  '```\n\n\n\n'
-].join('')
 
 
 class CreateTask extends Component {
@@ -39,9 +22,10 @@ class CreateTask extends Component {
     super(props)
     this.state = {
       title: '',
-      tags: '',
+      tags: [],
       spec: specPlaceholder
     }
+
     this.onChangeTags = this.onChangeTags.bind(this)
   }
 
@@ -50,9 +34,9 @@ class CreateTask extends Component {
       this.setState({ title: value })
   }
 
-  onChangeTags(tags) {
-    const numTags = tags.split(',').length
-    if (numTags < 6)
+  onChangeTags(e, data) {
+    const tags = data.value
+    if (tags.length < 6)
       this.setState({ tags })
   }
 
@@ -75,301 +59,100 @@ class CreateTask extends Component {
     } = this.state
 
     if (pendingTask) {
-      return <Redirect to={taskUrl(pendingTask)} />
+      //  TODO probably redirect to /tasks
+      //  This does not allow navigating to /tasks
+      //  then navigating to CreateTask while !!pendingTask
+      return <Redirect to={taskUrl(pendingTask)}/>
     }
 
     return (
       <Layout>
         <Head title='Create Task'/>
-        <div className='create-task-container'>
-          <div>
-            <h1 className='center'>Create Task</h1>
-            <form className='proposal-form' onSubmit={this.onSubmit}>
-              <div className='create-task-row-title'>
-                <input
-                  type='text'
-                  placeholder='Title'
-                  onChange={this.onChangeTitle}
-                  className='title'
-                  name='title'
-                  value={title}
-                />
-              </div>
-              <div className='create-task-row-tags'>
-                <Select
-                  className='react-select'
-                  optionClassName='select-option'
-                  clearable={false}
-                  name='react-select'
-                  simpleValue={true}  // return a string and not an array entire object
-                  smartIndent={true}
-                  multi={true}
-                  placeholder='Tags'
-                  backspaceToRemoveMessage='' // helper message we don't want displayed
-                  options={[
-                    {
-                      value: 'frontend-tests',
-                      label: 'Frontend Tests'
-                    }, {
-                      value: 'contract-tests',
-                      label: 'Contract Tests'
-                    }, {
-                      value: 'research',
-                      label: 'Research'
-                    }, {
-                      value: 'twitter',
-                      label: 'Twitter'
-                    }, {
-                      value: 'solidity',
-                      label: 'Solidity'
-                    }, {
-                      label: 'DID',
-                      value: 'did'
-                    }, {
-                      label: 'HAV',
-                      value: 'hav'
-                    }, {
-                      label: 'Tokens',
-                      value: 'tokens'
-                    }, {
-                      label: 'React',
-                      value: 'react'
-                    }, {
-                      label: 'HTML',
-                      value: 'html'
-                    }, {
-                      label: 'CSS',
-                      value: 'css'
-                    }, {
-                      label: 'Ideas',
-                      value: 'ideas'
-                    }, {
-                      label: 'Governance',
-                      value: 'governance'
-                    }, {
-                      label: 'Code Review',
-                      value: 'code-review'
-                    }, {
-                      label: 'Applications',
-                      value: 'applications'
-                    }, {
-                      label: 'Security',
-                      value: 'security'
-                    }, {
-                      label: 'Contracts',
-                      value: 'contracts'
-                    }, {
-                      label: 'Website',
-                      value: 'website'
-                    }, {
-                      label: 'Social',
-                      value: 'social'
-                    }, {
-                      label: 'Administration',
-                      value: 'admin'
-                    }, {
-                      label: 'Decisions',
-                      value: 'decisions'
-                    }, {
-                      label: 'Design',
-                      value: 'design'
-                    }, {
-                      label: 'Open Source',
-                      value: 'open-source'
-                    }, {
-                      label: 'Meetups',
-                      value: 'meetups'
-                    }, {
-                      label: 'Education',
-                      value: 'education'
-                    }, {
-                      label: 'Contributors',
-                      value: 'contributors'
-                    }, {
-                      label: 'Voting Dapp',
-                      value: 'voting-dapp'
-                    }, {
-                      label: 'Planning',
-                      value: 'planning'
-                    }, {
-                      label: 'Task Management',
-                      value: 'tasks'
-                    }, {
-                      label: 'Legal',
-                      value: 'Legal'
-                    }, {
-                      label: 'Crowdsale',
-                      value: 'crowdsale'
-                    }
-                  ]}
-                  value={tags}
-                  //TODO onBlur close tags input (instead of just onClick outside now
-                  onChange={this.onChangeTags}
-                />
-              </div>
-              <div className='create-task-row-spec'>
+        <div className='ui one column grid'>
+          <form className='ui form' onSubmit={this.onSubmit}>
+            <h1 className='ui header'>Create Task</h1>
+            <div className='field'>
+              <input
+                type='text'
+                placeholder='Title'
+                onChange={this.onChangeTitle}
+                className=''
+                name='title'
+                value={title}
+              />
+            </div>
+            <div className='field'>
+              <Dropdown
+                // compact
+                fluid
+                multiple
+                onChange={this.onChangeTags}
+                options={tagsOptions}
+                placeholder='Tags'
+                search
+                selection
+                scrolling
+                value={tags}
+              />
+            </div>
+            <div className='fields fields-margin-fix'>
+              <div className='eight wide field'>
                 <CodeMirror value={spec}
                   options={{
                     cursorBlinkRate: 650,
                     lineNumbers: true,
+                    lineWrapping: true,
                     mode: 'markdown',
-                    tabSize: 2,
-                    lineWrapping: true
+                    scrollbarStyle: null,
+                    tabSize: 2
                   }}
                   onValueChange={this.onChangeSpec}
                 />
-                <div className='create-task-spec-preview'>
-                  <ReactMarkdown source={spec}/>
-                </div>
               </div>
-              <div className='submit-button center'>
-                <button className='button' type='submit'>
-                  Submit
-                </button>
+              <div className='eight wide field react-markdown'>
+                <ReactMarkdown source={spec} />
               </div>
-            </form>
-          </div>
+            </div>
+            <button className='ui large green button' type='submit'>
+              Submit
+            </button>
+          </form>
         </div>
 
         { /*language=CSS*/ }
         <style global jsx>{`
 
-          .create-task-container {
-            display: flex;
-            flex-direction: column;
-            flex-shrink: 0;
-          }
-
-          .create-task-row-title {
-            margin-top: 25px;
-            text-align: center;
-            min-height: 50px;
-          }
-
-          .create-task-row-tags {
-            border-bottom: 1px solid #ccc;
-            border-radius: 3px;
-            margin: 15px 0;
-            min-height: 00px;
-            padding: 15px;
-            text-align: center;
-          }
-
-          input.title {
-            border-bottom: 1px solid #ccc;
-            box-sizing: border-box;
-            color: #333;
-            font-size: 22px;
-            height: 40px;
-            width: 100%;
-            letter-spacing: 2px;
-            text-align: center;
-          }
-
-          .title::placeholder {
-            color: lightgray;
-            font-size: 22px;
-            letter-spacing: 2px;
-            text-align: center;
-          }
-
-          .react-select {
-            box-sizing: border-box;
-            color: #333;
-            font-size: 22px;
-            letter-spacing: 2px;
-            text-align: center;
-            margin-right: -20px;
-          }
-
-          .Select-multi-value-wrapper {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            justify-content: center;
-          }
-
-          .Select-value, .select-option {
-            background-color: lightgray;
-            border: 1px solid #ccc !important;
-            border-radius: .25rem;
-            display: inline-block;
-            font-weight: 400;
-            line-height: 1.25;
-            margin: 2px;
-            padding: .4rem .6rem;
-            font-size: 1rem;
-            text-align: center;
-            text-transform: none;
-            vertical-align: middle;
-            user-select: none;
-            white-space: nowrap;
-          }
-
-          .create-task-row-spec {
-            display: flex;
-            flex-direction: row;
-            margin-top: 20px;
-            height: 420px;
-          }
-
-          .create-task-row-spec > div {
-            width: 50%;
+          .react-codemirror2, .react-markdown {
+            border: 1px solid rgba(34,36,38,.15);
+            border-radius: .28571429rem;
           }
 
           .react-codemirror2 {
-            border: 1px solid #ccc;
-            border-radius: 3px;
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+            margin-right: -.5rem;
+          }
+
+          .react-markdown {
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+            padding: 1rem;
+          }
+
+          .fields-margin-fix {
+            margin: 0 -.5em 1em !important;
+          }
+
+          .CodeMirror {
+            height: 36rem;
+          }
+
+          .CodeMirror-scroll {
+            padding-bottom: 0;
           }
 
           .center {
             text-align: center;
-          }
-
-          .Select-menu-outer {
-            margin-top: 5px;
-          }
-
-          .create-task-spec-preview {
-            border: 1px solid #ccc;
-            padding: 10px 15px 3px 15px;
-          }
-
-          .Select-value-icon {
-            margin-right: 4px;
-            cursor: pointer;
-          }
-
-          .word-separator {
-            margin: 0 6px;
-          }
-
-          .Select-placeholder {
-            color: lightgray;
-            font-size: 22px;
-            letter-spacing: 2px;
-            text-align: center;
-          }
-
-          .submit-button {
-            margin-top: 23px;
-          }
-
-          .button {
-            background: #4ad934;
-            background-image: linear-gradient(to bottom, #4ad934, #4eb82b);
-            border-radius: 5px;
-            text-shadow: 2px 1.5px 2px #666666;
-            color: #ffffff;
-            width: 20%;
-            font-size: 18px;
-            padding: 10px 20px;
-            text-decoration: none;
-          }
-
-          .button:hover {
-            background: #3ba52b;
-            text-decoration: none;
           }
 
           .underlined {
@@ -377,7 +160,7 @@ class CreateTask extends Component {
           }
 
           .CodeMirror-cursor {
-            border-left: 8px solid #ccc;
+            border-left: .5rem solid #ccc;
           }
 
         `}</style>
