8636d783316f3a40be69af7f910d033ae0a66dbd
JohnAllen <johnjalleniii@gmail.com>
2017-09-20 19:04:22 -0700

    Fix broken contracts and revert to self-hosted testnet -- that works :) Remove unused image

--- truffle.js
+++ truffle.js
@@ -1,27 +1,16 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*"
+      network_id: '*',
     },
     distnet: {
-      host: "localhost",
-      port: 8545,
-      network_id: 15
-    },
-    ropsten: {
-      host: "localhost",
-      port: 8545,
-      network_id: 3,
-      gas: 4500000
-    },
-    distnetRemote: {
       host: '165.227.28.206',
       port: 9000,
       network_id: 9000,
       from: '0xa16d8dd51010744954ec49c6f75a761ea60095d4',
-      gas: 4650000
-    }
-  }
+      gas: 4650000,
+    },
+  },
 }
