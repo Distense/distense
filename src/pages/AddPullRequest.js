940db903d18b68875440c346f5c27299754f1050
JohnAllen <johnjalleniii@gmail.com>
2018-01-10 23:51:05 -0800

    Add draft helper text to AddPullRequest

--- src/pages/AddPullRequest.js
+++ src/pages/AddPullRequest.js
@@ -7,9 +7,13 @@ import {
   Grid,
   Input,
   Header,
-  Message
+  List,
+  Message,
 } from 'semantic-ui-react'
-import { Redirect } from 'react-router-dom'
+import {
+  Link,
+  Redirect
+} from 'react-router-dom'
 
 import { fetchTask, selectTask } from '../actions/tasks'
 import { getSelectedTask } from '../reducers/tasks'
@@ -49,7 +53,7 @@ class AddPullRequest extends Component {
     }
   }
 
-  onChangePRNum= ({ target: { value } }) => {
+  onChangePRNum = ({ target: { value } }) => {
     this.setState({ prNum: value })
   }
 
@@ -69,48 +73,93 @@ class AddPullRequest extends Component {
     const { taskId, prNum, redirect } = this.state
 
     if (redirect) {
-      return <Redirect to="/pullrequests" />
+      return <Redirect to="/pullrequests"/>
     }
 
     return (
       <Layout>
-        <Head title="Task" />
+        <Head title="Task"/>
         <div className="task">
           <Grid columns={1}>
-          <Grid.Row width={4} columns={1}>
-            <Form onSubmit={this.onSubmit}>
-              <Header as="h1">Submit Pull Request</Header>
-              <Form.Field required>
-                <Input
-                  label="Pull Request Number"
-                  type="text"
-                  placeholder=""
-                  onChange={this.onChangePRNum}
-                  name="url"
-                  value={prNum}
-                />
-              </Form.Field>
-              <Form.Field required>
-                <Input
-                  label="Task Id"
-                  name="id"
-                  onChange={this.onChangeTaskId}
-                  placeholder="hash"
-                  type="text"
-                  value={taskId}
-                />
-              </Form.Field>
-              <Button size="large" color="green" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Grid.Row>
+            <Grid.Row width={4} columns={1}>
+              <Form onSubmit={this.onSubmit}>
+                <Header as="h1">Submit Pull Request</Header>
+                <Form.Field required>
+                  <Input
+                    label="Pull Request Number"
+                    type="text"
+                    placeholder=""
+                    onChange={this.onChangePRNum}
+                    name="url"
+                    value={prNum}
+                  />
+                </Form.Field>
+                <Form.Field required>
+                  <Input
+                    label="Task Id"
+                    name="id"
+                    onChange={this.onChangeTaskId}
+                    placeholder="hash"
+                    type="text"
+                    value={taskId}
+                  />
+                </Form.Field>
+                <Button size="large" color="green" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Grid.Row>
+            <Message>
+              <Message.Header>
+                Submit work in the form of a pull request on this page
+              </Message.Header>
+            </Message>
+
+            <Grid.Row>
+                <Message>
+                  <Message.Header>
+                    Pull Request number
+                  </Message.Header>
+                  <List bulleted>
+                    <List.Item>
+                      First submit a pull request on Github
+                    </List.Item>
+                    <List.Item>
+                      Enter the number found in your PR URL in the top box of this web page
+                    </List.Item>
+                    <List.Item>
+                      The number is the last part of your pull request URL. Example: https://github.com/Distense/distense-ui/pulls/<b>321</b>
+                    </List.Item>
+                    <List.Item>
+                      Enter just the number: 123
+                    </List.Item>
+                  </List>
+                </Message>
+              <Message>
+                <Message.Header>
+                  Task ID
+                </Message.Header>
+                <List bulleted>
+                  <List.Item>
+                    Find the Task ID of the task your PR completes
+                  </List.Item>
+                  <List.Item>
+                    The Task ID can be found by clicking the corresponding task found in the&nbsp;
+                    <Link to='/tasks'>tasks list</Link>
+                    &nbsp;page.
+                  </List.Item>
+                  <List.Item>
+                    If you know the URL of your task, the Task ID is the last part and will look like:
+                  </List.Item>
+                </List>
+              </Message>
+            </Grid.Row>
           </Grid>
           {task && (
             // Preview task info so submitter can verify they're submitting for
             // the right task
             <div>
-              <Divider section />
+              <Divider section/>
               <Header as="h3">Pull Request Task Verification</Header>
               <Message>
                 <Message.Header>Title: {task.title}</Message.Header>
@@ -133,4 +182,4 @@ const mapDispatchToProps = dispatch => ({
   selectTask: taskId => dispatch(selectTask(taskId))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddPullRequest )
+export default connect(mapStateToProps, mapDispatchToProps)(AddPullRequest)
