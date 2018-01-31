368391d217d01745e8acc9cd62577532b96d4dac
JohnAllen <johnjalleniii@gmail.com>
2018-01-31 10:55:18 -0800

    Update getRepoNumber test

--- src/test/tasks/getRepoNumber.test.js
+++ src/test/tasks/getRepoNumber.test.js
@@ -5,17 +5,16 @@ import {
   CONTRACTS_REPO_NAME
 } from '../../constants/repoNames'
 import { getRepoNumber } from '../../helpers/tasks/getRepoNumber'
-import { repoNameAndNumbers } from '../../constants/repoNameAndNumbers'
 
 describe('getRepoNumber', function() {
   it('should properly return the repo number for the frontend repo', function() {
-    const expected = repoNameAndNumbers[FRONTEND_REPO_NAME]
+    const expected = '0'
     const actual = getRepoNumber(FRONTEND_REPO_NAME)
-    assert.equal(expected, actual, 'Frontend repo should be 0')
+    assert.equal(expected, actual, `Frontend repo should be ${expected}`)
   })
 
   it('should properly return the repo number for the contracts repo', function() {
-    const expected = repoNameAndNumbers[CONTRACTS_REPO_NAME]
+    const expected = '1'
     const actual = getRepoNumber(CONTRACTS_REPO_NAME)
     assert.equal(expected, actual, 'Contracts repo should be 1')
   })
