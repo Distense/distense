2f52c79dc00982e2e13b674305989361e3445949
tanmayanand44 <anand44tanmay@gmail.com>
2018-02-03 23:50:08 +0530

    Added FAQ

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -69,12 +69,20 @@ export default class FAQ extends Component {
               <Header as="h3">What id DID?</Header>
             </p>
             <p style={{ fontSize: '1.10em' }}>
-              1.)DID are our economic and governance token
-              2.)They represent ownership of Distense
-              3.)They give the owner the right to: Propose/add tasks to Distense
-                                                   Vote on Distense <Link to="/Parameters">parameters</Link>
-                                                   Approve work
-                                                   Receive dividends if Distense earns money
+              1.)DID are our economic and {'\n'}governance token
+</p>
+            <p style={{ fontSize: '1.10em' }}>
+              2.)They represent ownership of Distense</p>
+            <p style={{ fontSize: '1.10em' }}>
+              3.)They give the owner the right to: 
+            </p>
+            <p style={{ fontSize: '1.10em' }}>i.Propose/add tasks to Distense
+            </p>
+            <p style={{ fontSize: '1.10em' }}>ii.Vote on Distense <Link to="/Parameters">parameters</Link>
+            </p>
+            <p style={{ fontSize: '1.10em' }}>iii.Approve work
+            </p>
+            <p style={{ fontSize: '1.10em' }}>iv.Receive dividends if Distense earns money
             </p>
             <p style={{ fontSize: '1.33em', marginTop: '2.2em' }}>
               <Header as="h3">Why should I contribute and earn DID?</Header>
