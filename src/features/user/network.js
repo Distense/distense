6e4217170fe8b5cebc95aafb3ce0c5f8dac85286
JohnAllen <johnjalleniii@gmail.com>
2018-04-03 17:09:16 -0700

    Update network to MAINNET

--- src/features/user/network.js
+++ src/features/user/network.js
@@ -1,3 +1,2 @@
-export const NETWORK = '3'
-export const NETWORK_NAME =
-  NETWORK === '1' ? 'MAINNET' : NETWORK === '3' ? 'ROPSTEN' : 'UNKNOWN'
+export const NETWORK = '1'
+export const NETWORK_NAME = 'Mainnet'
