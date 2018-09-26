e96bee754c6ef1293d94fd51f0b3d3566d037147
JohnAllen <johnjalleniii@gmail.com>
2018-09-26 16:18:58 -0700

    Update hardcoded MAINNET gas price to 8 gwei

--- src/features/user/gasPrices.js
+++ src/features/user/gasPrices.js
@@ -1,4 +1,4 @@
 import web3Utils from 'web3-utils'
 
 export const TESTNET_GAS_PRICE = web3Utils.toWei('25', 'gwei')
-export const MAINNET_GAS_PRICE = web3Utils.toWei('1', 'gwei')
+export const MAINNET_GAS_PRICE = web3Utils.toWei('8', 'gwei')
