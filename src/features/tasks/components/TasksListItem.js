c95904d3d27ff4d4173d2aaee15f8ba5581e2d80
John Allen <john@disten.se>
2019-07-27 17:38:54 -0700

    Revert "Tasks list item (#277)"
    This reverts commit 78d45675633e9a50893a9b8eeb1420429fd38d9b.

---
+++ src/features/tasks/components/TasksListItem.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Table } from 'semantic-ui-react'
+
+import Tags from './Tags'
+
+export const TasksListItem = ({ task }) => (
+  <Table.Row key={task._id}>
+    <Table.Cell>
+      <Link to={`/tasks/${task.title.split(' ').join('-')}/${task._id}`}>
+        {task.title}
+      </Link>
+    </Table.Cell>
+    <Table.Cell style={{ whiteSpace: 'nowrap' }}>
+      <Tags tags={task.tags} />
+    </Table.Cell>
+    <Table.Cell>{task.rewardStatus}</Table.Cell>
+    <Table.Cell style={{ whiteSpace: 'nowrap' }}>
+      {task.rewardString}
+    </Table.Cell>
+    <Table.Cell style={{ whiteSpace: 'nowrap' }}>
+      {task.votingStatus}
+    </Table.Cell>
+    <Table.Cell collapsing textAlign="right">
+      {task.createdAt && task.createdAt.toDateString()}
+    </Table.Cell>
+    <Table.Cell>
+      <Button
+        basic
+        color="green"
+        compact={true}
+        floated="right"
+        fluid={true}
+        size="mini"
+        as={Link}
+        title="Submit work for this task"
+        to={`/pullrequests/add/${task._id}`}
+      >
+        Submit PR
+      </Button>
+    </Table.Cell>
+  </Table.Row>
+)
